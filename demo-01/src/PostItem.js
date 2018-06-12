8931a93ded1f81352393c1e68870827122ba169a
BeijiYang <langyuyang@outlook.com>
2018-06-12 16:00:54 +0800

    state of postitem

--- demo-01/src/PostItem.js
+++ demo-01/src/PostItem.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class PostItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vote: 0
+    };
+  }
+
+  handleClick() {
+    let vote = this.state.vote;
+    vote++;
+    this.setState({ vote });
+  }
+
   render() {
     const { title, time, author } = this.props;
     return (
@@ -11,6 +24,16 @@ class PostItem extends Component {
         </div>
         <div>
           time: <span>{time}</span>
+        </div>
+        <div>
+          {" "}
+          <button
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            thumbUp
+          </button>: {this.state.vote}
         </div>
       </li>
     );
